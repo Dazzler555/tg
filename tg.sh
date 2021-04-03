895b505602881435e10c6538462b15ddf9e8e547
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2021-04-03 21:23:34 +0530

    tg

---
+++ tg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/out/target/product/violet
+
+curl -F chat_id=1033360588 -F document=*zip -F parse_mode=markdown https://api.telegram.org/bot1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U/sendDocument
+curl -F chat_id=1033360588 -F document=recovery.img -F parse_mode=markdown https://api.telegram.org/bot1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U/sendDocument
